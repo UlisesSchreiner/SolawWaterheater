81adf14e5b46875bc1660ae500b74364d99906e4
UlisesSchreiner <ulyvelez97@hotmail.com>
2019-05-19 00:13:04 -0300

    v1

--- FW-328p/FW-328p/FW-328p.ino
+++ FW-328p/FW-328p/FW-328p.ino
@@ -26,9 +26,9 @@ Thread ControlRele = Thread();
 class Termotanque
 {
   private:
-          byte estadoRele;
-          byte estadoConexion;
-          int contadorRele;
+          byte estadoRele;     // rele on/off 
+          byte estadoConexion; // con/sin internet
+          int contadorRele;    // 
           int contadorTemperatura; 
           int contadorWDserial;    
           float temperaturaGlobal;
@@ -41,21 +41,25 @@ class Termotanque
           void sumarContRele(){contadorRele++;}
           int GetContRele(){return contadorRele;}
           int GetContadorWDserial(){return contadorWDserial;}
-          void GetTemp()
+          
+          void GetTemp() // obtiene la temp y rsetea el cont temp
           {
             sensors.requestTemperatures();
             temperaturaGlobal = sensors.getTempCByIndex(0);
             contadorTemperatura = 0;
-            }
-            void setEstadoConexion(byte estCone)
-            {
+          }
+          
+          void setEstadoConexion(byte estCone) // fijar estado conexion 
+          {
               estadoConexion = estCone;
-            }
-            void setEstadoRele(byte estRele)
+          }
+          
+          void setEstadoRele(byte estRele) // fijar estado rele
             {
               estadoRele = estRele;
             }
-           void sendEstado()
+            
+          void sendEstado() // enviar estado al ESP, suma WD serial
            {
                String temp = String(temperaturaGlobal);
                String data = "{ \"estTemp\": \"" + temp + "\"}";
@@ -63,11 +67,12 @@ class Termotanque
                Serial.println(data);
                sumarWDserial();
            }
-           void Rele()
+           
+          void Rele() // prende o apaga el rele y resetea contador rele
            {
-            if(temperaturaGlobal > 0 && temperaturaGlobal < 65)
+            if(temperaturaGlobal > 0 && temperaturaGlobal < 65) // si esta dentro del rango 0-65 ->
             {
-              if(estadoRele == 1)
+              if(estadoRele == 1) // y ademas el estado es encendido ->
               {
                 digitalWrite(RelePin, HIGH);
               }else{digitalWrite(RelePin, LOW);}
@@ -91,7 +96,7 @@ void setup(void) {
   pinMode(DallasPin,INPUT);
   digitalWrite(resetPin, LOW);
 
-  CadaUnMinuto.onRun(cadaUnMinuto);
+  CadaUnMinuto.onRun(cadaUnSegundo);
   ControlRele.onRun(controlRele);
 }
 
@@ -102,50 +107,55 @@ void loop() {
 
   if(WIFI.available())
       {
-        String input = "";
-        while(WIFI.available())
-      {
-    char c = WIFI.read();
-    input = input + c;
-      }
-      Serial.println(input);
-  StaticJsonBuffer<50> jsonBuffer;
-  JsonObject& root = jsonBuffer.parseObject(input);
- 
- byte estRele = root["abc"];
- byte estCon = root["estConnection"];
-
- if(estCon != 0){
-     T.setEstadoConexion(estCon);
-     Serial.println(estCon);
- }
- if(estRele != 0){
-     T.setEstadoRele(estRele);
-     T.ResetWDserial();
-     Serial.println(estRele);
- }
+         String input = "";
+         while(WIFI.available())
+         {
+            char c = WIFI.read();
+            input = input + c;
+         }
+         Serial.println(input);
+         StaticJsonBuffer<50> jsonBuffer;
+         JsonObject& root = jsonBuffer.parseObject(input);
+     
+         byte estRele = root["abc"];
+         byte estCon = root["estConnection"];
+    
+        if(estCon != 0)
+        {
+         T.setEstadoConexion(estCon);
+         Serial.println(estCon);
+        }
+        
+        if(estRele != 0) // reser WD serial
+        {
+         T.setEstadoRele(estRele);
+         T.ResetWDserial();
+         Serial.println(estRele);
+         }
       }
 
   
-if(T.GetContTemp() > 20){ wdt_enable(WDTO_30MS); while(1){};}
-if(T.GetContadorWDserial() > 5){
-    digitalWrite(resetPin, HIGH);
-    delay(10);
-    digitalWrite(resetPin, LOW);}
-if(T.GetContRele() > 10){ wdt_enable(WDTO_30MS); while(1){};}
+  if(T.GetContTemp() > 20){ wdt_enable(WDTO_30MS); while(1){};}
+  if(T.GetContadorWDserial() > 5) //reser ESP
+  {
+      digitalWrite(resetPin, HIGH);
+      delay(10);
+      digitalWrite(resetPin, LOW);
+  }
+  if(T.GetContRele() > 10){ wdt_enable(WDTO_30MS); while(1){};}
 }
 
-void cadaUnMinuto()
+void cadaUnSegundo()
 {
-  T.GetTemp();
-  T.sendEstado();
+  T.GetTemp();    // reser cont temp
+  T.sendEstado(); // suma WD serial
   delay(1000);
 }
 
 void controlRele()
 {
-  T.Rele();
-  T.SumarContTemp();
-  T.sumarContRele();
+  T.Rele();            //resetea cont rele
+  T.SumarContTemp();   // suma cont temp
+  T.sumarContRele();   // suma cont rele 
   delay(250);
 }
