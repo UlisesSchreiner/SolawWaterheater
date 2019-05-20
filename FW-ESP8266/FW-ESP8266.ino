78db1e952ceb460868a33f3497b78a1a1a84072c
UlisesSchreiner <ulyvelez97@hotmail.com>
2019-05-20 01:05:06 -0300

    first commit

--- FW-ESP8266/FW-ESP8266.ino
+++ FW-ESP8266/FW-ESP8266.ino
@@ -4,13 +4,14 @@
 //#include <ESP8266WebServer.h>
 //#include <ESP8266mDNS.h>
 #include <ArduinoJson.h>
-#include <EEPROM.h>
+
 #include <Thread.h>
 #include <ThreadController.h>
 #include <NTPClient.h>
 #include <WiFiUdp.h>
 #include <TimeLib.h>
 #include <PubSubClient.h>
+#include "Eeprom.cpp"
 
 //const char* ssid = "WiFi-Arnet-2jdz";
 //const char* password = "rxire3p8";
@@ -191,43 +192,6 @@ estadoConexion = WiFi.status();
 };
 
 Termotanque T;
-class Eeprom
-{
-  private:
-           int addrSsid = 0;
-           //tamaño ssid = EEPROM.read(21);
-  public:
-          void grabar(int addr, String a) {
-            EEPROM.begin(500);
-  int tamano = a.length(); 
-  char inchar[50]; 
-  a.toCharArray(inchar, tamano+1);
-  for (int i = 0; i < tamano; i++) {
-    EEPROM.write(addr+i, inchar[i]);
-  }
-  for (int i = tamano; i < 50; i++) {
-    EEPROM.write(addr+i, 255);
-  }
-  EEPROM.commit();
-  //EEPROM.end();
-}
-
-
-String leer(int addr) {
-  EEPROM.begin(500);
-   byte lectura;
-   String strlectura;
-   for (int i = addr; i < addr+50; i++) {
-      lectura = EEPROM.read(i);
-      if (lectura != 255) {
-        strlectura += (char)lectura;
-      }
-   }
-   return strlectura;
-   EEPROM.end();
-}
-
-};
 
 Eeprom E;
 
